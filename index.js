2bca83a58d92d7da01e18cfb4a0de558d8342ea1
Antal <kotiantalg@gmail.com>
2021-08-07 18:12:51 +0200

    Removed cluster & pre-render

--- index.js
+++ index.js
@@ -1,81 +1,53 @@
 const express = require('express');
 const redis = require('redis');
-const sitemapper = require('sitemapper');
 const puppeteer = require('puppeteer');
-const CronJob = require('cron').CronJob;
-const { Cluster } = require('puppeteer-cluster');
 
 require('dotenv').config();
 
 const redisClient = redis.createClient();
 const app = express();
-const sitemap = new sitemapper();
+let browser = undefined;
 
-let cluster = undefined;
-
-let isRunning = false;
-
-var job = new CronJob(
-    '0 20 3 * * *',
-    () => {
-        startProcess();
-    },
-    null,
-    true,
-);
-
-async function init() {
-    cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 5,
-    });
-    await cluster.task(async ({ page, data: url }) => {
-        try {
-            await page.goto(url, {
-                waitUntil: 'networkidle2',
-                timeout: 180 * 1000,
-            });
-            const content = await page.content();
-            const status = await redisClient.set(
-                `prerender:${url}`,
-                content,
-            );
-            console.log(url, status, content.length);
-        } catch (e) {
-            console.log(e);
-        }
-    });
-}
 
 app.get('/url/*', async (req, res) => {
     console.log(req.params[0]);
-    redisClient.get(`prerender:${req.params[0]}`, (err, content) => {
-        res.send(content);
-    });
-});
-
-app.get('/start', async (req, res) => {
-    startProcess();
-    res.send('Process Started');
-});
-
-async function startProcess() {
-    if (isRunning) {
-        return false;
-    }
-
-    isRunning = true;
-    await init();
-    sitemap.fetch(process.env.SITEMAP).then(async (data) => {
-        for await (const site of data.sites) {
-            await cluster.queue(site);
+    redisClient.get(`prerender:${req.params[0]}`, async (err, content) => {
+        if (!content) {
+            const response = await preRender(req.params[0]);
+            res.send(response);
+        } else {
+            res.send(content);
         }
-        await cluster.idle();
-        await cluster.close();
-        isRunning = false;
     });
-}
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening at http://localhost:${process.env.PORT}`);
 });
+
+async function preRender(url) {
+    const content = await fetchPage(url);
+    await redisClient.set(
+        `prerender:${site}`,
+        content,
+    );
+    return content;
+}
+
+async function fetchPage(url) {
+    if (!browser) {
+        throw 'Failed to initialize puppeteer';
+    }
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'networkidle2',
+        timeout: 180 * 1000,
+    });
+    return await page.content();
+}
+
+async function init() {
+    browser = await puppeteer.launch();
+}
+
+init();
